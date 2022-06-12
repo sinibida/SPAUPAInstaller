8197125113f8170d94f76431bb5683c14fe9e604
SPAUPA <40449398+sinibida@users.noreply.github.com>
2022-06-12 18:35:58 +0900

    Some Jobs

---
+++ SIConsole/Jobs/DeleteDirectoryJob.cs
@@ -0,0 +1,34 @@
+﻿namespace SIConsole.Jobs;
+
+[SIJobInfo(Code = "delete_dir")]
+public class DeleteDirectoryJob : SIJob
+{
+    public string Dir { get; set; }
+
+    public override void Execute()
+    {
+        var expDir = Environment.ExpandEnvironmentVariables(Dir);
+        if (Directory.Exists(expDir))
+        {
+            var files = Directory.GetFiles(expDir, "*", SearchOption.AllDirectories);
+            var dirs = Directory.GetDirectories(expDir, "*", SearchOption.AllDirectories);
+            int total = files.Length + dirs.Length + 1;
+            int i = 0;
+            foreach (var path in files)
+            {
+                File.Delete(path);
+                i++;
+                Progress = (double)i / total;
+            }
+            foreach (var path in dirs)
+            {
+                Directory.Delete(path);
+                i++;
+                Progress = (double)i / total;
+            }
+            Directory.Delete(expDir);
+        }
+        
+        Progress = 1;
+    }
+}
---
+++ SIConsole/Jobs/DeleteFIleJob.cs
@@ -0,0 +1,15 @@
+﻿namespace SIConsole.Jobs;
+
+[SIJobInfo(Code = "delete")]
+public class DeleteFileJob : SIJob
+{
+    public string Filename { get; set; }
+
+    public override void Execute()
+    {
+        var expFilename = Environment.ExpandEnvironmentVariables(Filename);
+        if (File.Exists(Filename))
+            File.Delete(expFilename);
+        Progress = 1;
+    }
+}
